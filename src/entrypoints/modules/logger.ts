3f331156d98213b0eacdb47376a20d8dc1722fb6
Said <sa3id@live.com>
2025-07-08 00:30:49 +0100

    ✨ feat(logger): implement logger class with enable/disable functionality
    - create a logger class with enable/disable functionality
    - add log method to conditionally log messages based on the enabled state

---
+++ src/entrypoints/modules/logger.ts
@@ -0,0 +1,20 @@
+export class Logger {
+  private _enabled: boolean;
+
+  constructor(enabled?: boolean) {
+    this._enabled = enabled || true;
+  }
+  public get enabled(): boolean {
+    return this._enabled;
+  }
+
+  public set enabled(value: boolean) {
+    this._enabled = value;
+  }
+
+  public log(...args: any[]) {
+    if (this._enabled) {
+      console.log(...args);
+    }
+  }
+}
